141557cd03113f0d773f9525f911b04b638576eb
samarthmadalageri <samarthmadalageri@gmail.com>
2023-11-03 17:30:30 +0530

    modify two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -5,18 +5,18 @@
 # You can return the answer in any order.
 
 # Example 1:
-
 # Input: nums = [2,7,11,15], target = 9
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-# Example 2:
 
+# Example 2:
 # Input: nums = [3,2,4], target = 6
 # Output: [1,2]
 
 # Date:03/11/23
 
 # we use hashmap to store values and their index
+# then we search for the second value of the pair by seeing if it's diff with target exist in the hashmap
 # enumerate function gives us the value and it's index
 
 class Solution:
